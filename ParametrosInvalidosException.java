35e09c3d6655bd37724bb46f2dadcba23aed3099
Cleberson Rizzo <114303047+clebersonrizzo@users.noreply.github.com>
2024-06-12 08:04:35 -0300

    Add files via upload

---
+++ ParametrosInvalidosException.java
@@ -0,0 +1,6 @@
+public class ParametrosInvalidosException extends Exception {
+	//classe de exceção customizada
+    public ParametrosInvalidosException(String mensagem) {
+        super(mensagem);
+    }
+}
